6aba619f1302905cb2ddcdcee113a931871c301a
viniciusbe <vinicius.bernardesds@gmail.com>
2025-08-11 22:55:30 -0300

    feat (header): add styles and svg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,26 @@
-import { Link } from '@tanstack/react-router'
+import { Link } from "@tanstack/react-router";
+import githubLogo from "../github-logo.svg";
+import githubNameLogo from "../github-name-logo.svg";
 
 export default function Header() {
   return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
+    <header className="flex items-center gap-2 bg-[#24292E] text-white h-[72px] ">
+      <div className="min-w-7xl m-auto px-4 flex items-center gap-5">
+        <div className="flex gap-4">
+          <img src={githubLogo} />
+          <img src={githubNameLogo} />
         </div>
-      </nav>
+        <div>
+          <span className="text-2xl">/</span>
+        </div>
+        <nav className="">
+          <ul className="">
+            <li>
+              <Link to="/">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
-  )
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute("/")({
 
 function App() {
   return (
-    <div className="min-h-[calc(100vh-40px)] flex flex-col items-center justify-center text-[calc(10px+2vmin)]">
+    <div className="min-h-[calc(100vh-72px)] flex flex-col items-center justify-center text-[calc(10px+2vmin)]">
       <h1>Github</h1>
     </div>
   );
